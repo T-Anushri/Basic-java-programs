60679ca99248eac4f3446a478ff383dd911c3a6f
Anushri T <142531710+T-Anushri@users.noreply.github.com>
2023-12-08 13:19:57 +0530

    Add files via upload

---
+++ Competitive programming/Flight.java
@@ -0,0 +1,41 @@
+import java.time.LocalTime;
+class Airplane{
+  private String flightNumber;
+  private String destination;
+  private LocalTime scheduledDeparture;
+  private int delayTime;
+  public Airplane(String flightNumber, String destination, LocalTime scheduledDeparture) {
+    this.flightNumber = flightNumber;
+    this.destination = destination;
+    this.scheduledDeparture = scheduledDeparture;
+    this.delayTime = 0;
+  }
+  public void delay(int minutes){
+    this.delayTime = minutes;
+    this.scheduledDeparture = this.scheduledDeparture.plusMinutes(minutes);
+  }
+  public void checkStatus(){
+    if (delayTime == 0) {
+      System.out.println("Flight " + flightNumber + " is on time.");
+    } else {
+      System.out.println("Flight " + flightNumber + " is delayed by " + delayTime + " minutes.");
+    }
+  }
+}
+public class Flight{
+  public static void main(String[] args){
+    Airplane flight1 = new Airplane("CDE345", "London", LocalTime.of(10, 30));
+    Airplane flight2 = new Airplane("KUI765", "New York", LocalTime.of(14, 0));
+    Airplane flight3 = new Airplane("JUY456", "Paris", LocalTime.of(14, 0));
+    System.out.println("Flight Status:");
+    flight1.checkStatus();
+    flight2.checkStatus();
+    flight3.checkStatus();
+    flight1.delay(40);
+    flight2.delay(110);
+    System.out.println("\nCurrent Flight Status:");
+    flight1.checkStatus();
+    flight2.checkStatus();
+    flight3.checkStatus();
+  }
+}
